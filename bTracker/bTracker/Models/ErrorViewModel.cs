d4a6f03f71bc45bfd699efd7d0f611edc5e691a8
PiaMalene <72134344+PiaMalene@users.noreply.github.com>
2023-07-28 01:42:29 +0200

    ASP.Net MVC framework setup

---
+++ bTracker/bTracker/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace bTracker.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
