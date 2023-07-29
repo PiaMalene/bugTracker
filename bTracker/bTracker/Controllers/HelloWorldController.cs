12996295c2699d1a811578ebed3a321c1be8ea60
PiaMalene <72134344+PiaMalene@users.noreply.github.com>
2023-07-29 04:12:41 +0200

    added basic views

--- bTracker/bTracker/Controllers/HelloWorldController.cs
+++ bTracker/bTracker/Controllers/HelloWorldController.cs
@@ -7,9 +7,9 @@ namespace bTracker.Controllers
     {
         // 
         // GET: /HelloWorld/
-        public string Index()
+        public IActionResult Index()
         {
-            return "This is my default action...";
+            return View();
         }
         // 
         // GET: /HelloWorld/Welcome/ 
